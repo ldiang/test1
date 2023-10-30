ffe7178add5d1104e50934d8016ade40c2eb6284
dxzhangjing <dxzhangjing@googlemail.com>
2023-10-30 01:15:41 +0100

    文章模型 视图 序列化器初步编写完毕，准备迁移

---
+++ article/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from cate_article.models import CateStore
+from users.models import UserStore
+
+
+# Create your models here.
+class ArticleStore(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    state = models.CharField(max_length=10,
+                             choices=[("已发布", "已发布"), ("草稿", "草稿")])
+    cover_img = models.ImageField(upload_to='img/', blank=True, null=True)
+    cate_id = models.ManyToManyField(CateStore)
+    author_id = models.ForeignKey(UserStore, on_delete=models.SET_NULL,
+                                  null=True)
+    pub_date = models.DateTimeField()
+
+    class Meta:
+        db_table = 'store_article'
+        verbose_name = '文章'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.title
